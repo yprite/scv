1212906dbe4fd86040a5f3140f82f7fc18644e9a
yprite <yprite@gmail.com>
2022-12-14 16:34:50 +0900

    fix: 로그 추가 및 변수 이름 변경 - target_price를 targetPrice로 변경하여 같으 의미는 하나의 단어로 통일 - elif에서 조건이 만족하는지 체크해보고자 else로 변경하여 조건검사를 하지않고 확인

--- scv.py
+++ scv.py
@@ -3,8 +3,9 @@ import json
 import os
 import pyupbit
 import numpy as np
+import datetime
 #https://techfox.tistory.com/59 pyupbit 라이브러리 추가
-# target_price = get_target_price("KRW-DOGE", 0.5)
+# targetPrice = get_target_price("KRW-DOGE", 0.5)
 # def get_target_price(ticker, k):
 #     """변동성 돌파 전략으로 매수 목표가 조회"""
 #     df = pyupbit.get_ohlcv(ticker, interval="day", count=20)
@@ -13,13 +14,16 @@ import numpy as np
 #     print(target_price)
 #     return target_price
 
-offlineMode=True
 
-def telegram_handler(event, context):
-    # TELEGRAM 메시지 보내
+offlineMode=True
+notiBackend="telegram"
+
+def telegram_handler(text):
+    # TELEGRAM 메시지 전송
     # https://velog.io/@dragontiger/%ED%8C%8C%EC%9D%B4%EC%8D%ACAWS-LambdaAWS-API-Gateway-%ED%85%94%EB%A0%88%EA%B7%B8%EB%9E%A8-%EB%B4%87-%EA%B0%9C%EB%B0%9C-%EB%B0%B0%ED%8F%AC%EA%B9%8C%EC%A7%80
     TELEGRAM_API_HOST = 'api.telegram.org'
     TOKEN = os.environ['TOKEN']
+    CHAT_ID = os.environ['CHAT_ID']
     
     connection = http.client.HTTPSConnection(TELEGRAM_API_HOST)
     
@@ -31,8 +35,8 @@ def telegram_handler(event, context):
     
     # 파라미터
     param = {
-        'chat_id': 491359217,
-        'text': 'python 에서 보냄'
+        'chat_id': CHAT_ID,
+        'text': text
     }
     
     # Http 요청
@@ -48,6 +52,14 @@ def telegram_handler(event, context):
     
     # 연결 끊기
     connection.close()
+
+def isTelegramRequest(event):
+    if "rawPath" in event:
+        return True
+    return False
+
+def post_message(text):
+    telegram_handler(text)
 
 def get_crr(df, fees, K) :
     df['range'] = df['high'].shift(1) - df['low'].shift(1)
@@ -106,24 +118,73 @@ def sell_all(coin) :
             print(upbit.sell_market_order(coin, balance))
         post_message("매도 체결.\n체결 단가 : "+str(pyupbit.get_current_price(coin))+" 원")
         
+def get_symbol(s):
+    symbolDict = {
+        "도지":"KRW-DOGE",
+        "이더":"KRW-ETH", "이더리움":"KRW-ETH", 
+        "비코":"KRW-BTC", "비트코인":"KRW-BTC", "비트":"KRW-BTC"
+    }
+    if s in symbolDict:
+        return symbolDict[s]
+        
 def upbit():
     coin = "KRW-DOGE"
     fees = 0.0005
+    startBalance = 1000000
     
     df = pyupbit.get_ohlcv(coin, count = 2, interval = "day")
     targetPrice = get_targetPrice(df, get_best_K(coin, fees))
-    print ("목표매수가 : ", targetPrice)
     currentPrice = pyupbit.get_current_price(coin)
-    print ("목표매수가: ",  targetPrice, "현재가: ", currentPrice)
-    if targetPrice <= currentPrice:
+    
+    curKrwBalance = get_balance("KRW")
+    curCoinBalance = get_balance(coin)
+    sumKrwBalance = curKrwBalance + (curCoinBalance*pyupbit.get_current_price(coin))
+        
+    post_message ("수익율: "+str(((sumKrwBalance/startBalance) -1)*100)+"%\n목표매수가: "+str(targetPrice)+"현재가: "+str(currentPrice)+"\n현재 잔고: "+str(get_balance("KRW")))
+    now = datetime.datetime.now()
+    if now.hour == 9:
+        print ("SELL 9시 now: " + now)
+        sell_all(coin)
+    # elif targetPrice <= currentPrice:
+    else 
+        print ("targetPrice: {}, currentPrice:{}".format(targetPrice, currentPrice))
         buy_all(coin)
 
+def parse_command(event):
+    print (event)
+    telRes = json.loads(event['body'])
+    text = telRes['message']['text']
+    if text[0] == '/':
+        splitText = text[1:].split()
+        cmd = splitText[0]
+        params = splitText[1:]
+        print ("cmd : ", cmd)
+        print ("params: ", params)
+        if cmd == "시세":
+            o = ""
+            idx = 0
+            for param in params:
+                symbol = get_symbol(param)
+                o += symbol
+                o += " : "
+                o += str(pyupbit.get_current_price(symbol))
+                if idx < len(params):
+                    o += "\n"
+            post_message(o)
+        
+    else:
+        post_message("지원하지않음 ")
+
 def lambda_handler(event, context):
-    # TODO implement
+    # TODO 상승장인지 아닌지 구분해야
     #telegram_handler(event, context)
-    upbit()
+    
+    if isTelegramRequest(event):
+        parse_command(event)
+    else:
+        upbit()
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello from scvBot on Lambda!')
     }
